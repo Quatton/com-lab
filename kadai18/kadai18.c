352def4a214427b4541584e2dad275b8c6b25de2
Quatton <atomicatives@gmail.com>
2023-10-03 21:05:20 +0900

    kadai18

---
+++ kadai18/kadai18.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/* 関数の定義を修正 */
+
+void fourArithmeticOperations(double* x, double* y, double* add, double* sub,
+                              double* mul, double* div) {
+  *add = *x + *y;
+
+  *sub = *x - *y;
+
+  *mul = *x * *y;
+
+  *div = *x / *y;
+};
+
+int main(void) {
+  double a[2] = {0.0, 0.0};
+
+  double add = 0.0;
+
+  double sub = 0.0;
+
+  double mul = 0.0;
+
+  double div = 0.0;
+
+  int i;
+
+  for (i = 0; i < 2; i++) scanf("%lf", &a[i]);
+
+  /* 四則演算の関数 */
+
+  fourArithmeticOperations(&a[0], &a[1], &add, &sub, &mul, &div);
+
+  printf("%lf\n %lf\n %lf\n %lf\n", add, sub, mul, div);
+
+  return 0;
+}
