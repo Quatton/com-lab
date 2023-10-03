a05332d4ca62d3acd2fb4b56664bc0f9514439d6
Quatton <atomicatives@gmail.com>
2023-10-03 21:25:50 +0900

    kadai20

---
+++ kadai20/kadai20.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void set_idx(int* a, int n) {
+  for (int i = 0; i < n; i++) *(a + i) = i;
+}
+
+// clang-format off
+int main(void){
+
+  int a[5];
+  int i;
+
+  set_idx(a, 5);
+
+  for (i = 0; i < 5; i++)
+      printf("%d\n", a[i]);
+
+  return 0;
+}
