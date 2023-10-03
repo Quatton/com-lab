ca7d08e22024f6b48b3b0cbe7a155bde2f36e34c
Quatton <atomicatives@gmail.com>
2023-10-03 22:24:03 +0900

    kadai26

---
+++ kadai26/kadai26.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+  char a[100], b[100];
+
+  scanf("%99s", a);
+  scanf("%99s", b);
+
+  char *pa = a, *pb = b;
+
+  while (*pa != '\0' && *pb != '\0') {
+    if (*pa == '\0' || *pb == '\0' || *pa != *pb) {
+      printf("NG");
+      return 0;
+    }
+
+    pa++;
+    pb++;
+  }
+
+  printf("OK");
+
+  return 0;
+}
