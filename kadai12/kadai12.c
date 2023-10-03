7e2e759eaf57689cedd29a953e365bb61d0a596a
Quatton <atomicatives@gmail.com>
2023-10-03 20:17:15 +0900

    fixed some formatting issue

--- kadai12/kadai12.c
+++ kadai12/kadai12.c
@@ -2,20 +2,24 @@
 
 int max_of(int* a, int n) {
   int max = 0;
-  for (int i = 0; i < n; i++)
-    if (a[i] > max) max = a[i];
+  for (int i = 0; i < n; i++) {
+    if (a[i] > max) {
+      max = a[i];
+    }
+  }
   return max;
 }
 
-int main(void) {
-  int a[5] = {0, 0, 0, 0, 0};
+// clang-format off
+int main(void){
 
+  int a[5] = {0, 0, 0, 0, 0};
   int i, max;
 
-  for (i = 0; i < 5; i++) scanf("%d", &a[i]);
+  for (i = 0; i < 5; i++)
+      scanf("%d", &a[i]);
 
   max = max_of(a, 5);
-
   printf("%d\n", max);
 
   return 0;
