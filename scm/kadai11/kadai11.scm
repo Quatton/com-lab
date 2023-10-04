b5e28b7d3538a1b9b358ea92992c97206aa5172c
Quatton <atomicatives@gmail.com>
2023-10-04 14:04:12 +0900

    reducer new

--- scm/kadai11/kadai11.scm
+++ scm/kadai11/kadai11.scm
@@ -1,12 +1,8 @@
 (define (reducer acc lst)
   (if (null? lst); (reducer (5 4 3 2 1) ())
     acc; (5 4 3 2 1)
-
-    (if (null? acc); (reducer () (1 2 3 4 5))
-      (reducer (list (car lst)) (cdr lst))
-      ; (reducer (3 2 1) (4 5))
-      (reducer (cons (car lst) acc) (cdr lst))
-    )
+    ;(3 2 1) (4 5)
+    (reducer (cons (car lst) acc) (cdr lst))
   )
 )
 
@@ -14,4 +10,4 @@
   (reducer () lst)
 )
 
-(my-reverse '(1 2 3))
+(my-reverse '(1 2 3 4 5))
