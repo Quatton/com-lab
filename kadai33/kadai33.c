7736ad526d03c9ee1d09bc81a92117e042fca761
Quatton <atomicatives@gmail.com>
2023-10-04 09:08:33 +0900

    メモリ領域が確保されなかった時に文字列をコピーしたり

--- kadai33/kadai33.c
+++ kadai33/kadai33.c
@@ -6,6 +6,9 @@ int main(void) {
   char *src1 = "Hello,world!";
   char *src2 = calloc(strlen(src1) + 1, sizeof(char));
 
+  if (src2 == NULL) {
+    exit(1);
+  }
   strcpy(src2, src1);
 
   printf("%s\n", src2);
