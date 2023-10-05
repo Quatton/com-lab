28b62fd80c68fcc2e5d4619e52cc89d7caa9d01a
Quatton <atomicatives@gmail.com>
2023-10-05 14:14:27 +0900

    kadai22

--- S/kadai21/kadai21.scm
+++ S/kadai21/kadai21.scm
@@ -3,7 +3,7 @@
 (define (my-stream-map f st)
   (if (stream-null? st)
       stream; empty stream
-      (cons-stream (f (stream-car st))
+      (cons-stream (f (stream-car st)) ;would do the same with list
                   (my-stream-map f (stream-cdr st)))))
 
 (define egg-map (my-stream-map (lambda (x) (* x x)) egg))
---
+++ S/kadai22/kadai22.scm
@@ -0,0 +1,34 @@
+(define (stream-reducer acc curr f)
+  (cond ((stream-null? curr) acc)
+        (else (stream-reducer (f acc (stream-car curr)) (stream-cdr curr) f))
+  )
+)
+
+(define (my-stream-map f st)
+  (if (stream-null? st)
+      stream; empty stream
+      (cons-stream (f (stream-car st)) ;would do the same with list
+                  (my-stream-map f (stream-cdr st)))))
+
+
+; partial sum
+; 1 2 3 4 5 
+; 1 1+2 1+2+3
+
+(define (plus x) 
+  (lambda (y) (+ x y))
+)
+
+(define (partial-sums-helper s acc)
+  (if (stream-null? s) 
+    stream
+    (cons-stream (+ acc (stream-car s))
+                (partial-sums-helper (stream-cdr s) (+ acc (stream-car s)))
+    )
+  )
+)
+
+(define (partial-sums s) (partial-sums-helper s 0))
+
+(stream-car (partial-sums (stream 1 2 3 4 5)))
+(stream-ref (partial-sums (stream 1 2 3 4 5)) 3)
