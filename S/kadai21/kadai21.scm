ca4ab5941c03b6926088f93f570a2f46c675651d
Quatton <atomicatives@gmail.com>
2023-10-05 13:44:38 +0900

    kadai21

---
+++ S/kadai21/kadai21.scm
@@ -0,0 +1,12 @@
+(define egg (list->stream (list 1 2 3 4)))
+
+(define (my-stream-map f st)
+  (if (stream-null? st)
+      stream; empty stream
+      (cons-stream (f (stream-car st))
+                  (my-stream-map f (stream-cdr st)))))
+
+(define egg-map (my-stream-map (lambda (x) (* x x)) egg))
+
+(stream-car egg-map)
+(stream-ref egg-map 2)
