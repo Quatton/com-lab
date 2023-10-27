297c3bb880f2254cd723a0fa3503b94cb38ce02d
Quatton <atomicatives@gmail.com>
2023-10-27 18:35:45 +0900

    please

--- ASM/kadai5/kadai5.s
+++ ASM/kadai5/kadai5.s
@@ -1,11 +1,13 @@
 	.globl swap_gt
 
-swap_gt:
-  lw a3, 0(a0) # load x to a3
-  lw a4, 0(a1) # load y to a4
-  bgt a3, a4, .swap # *x > *y
+swap_gt: # void swap_gt(int *x, int *y)
+  lw a3, 0(a0) # a3 = *x
+  lw a4, 0(a1) # a4 = *y
+  
+  # if (a3 > a4) swap()
+  blt a3, a4, .no_swap
+  sw a4, 0(a0) # *x = a4
+  sw a3, 0(a1) # *y = a3
 
-.swap:
-  sw a4, 0(a0) # *x -> 
-  sw a3, 0(a1) # *y -> 
-  ret
+.no_swap:
+  jr ra
