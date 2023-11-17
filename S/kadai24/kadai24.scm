cac3a08b49cf81bce1f93bdd6af00f9dafd29f6d
Quatton <atomicatives@gmail.com>
2023-11-17 13:19:03 +0900

    kadai24

--- S/kadai24/kadai24.scm
+++ S/kadai24/kadai24.scm
@@ -1,21 +1,20 @@
+(import (rnrs) (srfi :41))
+
 (define triple-index
-  (stream-cons (list 1 1 1)
+  (stream-cons (list 0 0 0)
     (stream-map
       (lambda (x)
         (let ((i (car x)) (j (cadr x)) (k (caddr x)))
-          ;i++ したい jで抑えられている
-          (if (< i j)
-            ;then ok
-            (list (+ i 1) j k)
-            ;j++ first, but kで抑えられている
-            (if (< j k)
-              ;then ok
-              (list i (+ j 1) k)
-              ;then k++ first
-              (list i k (+ k 1 ))
+          ;k++ したいが、jがついて来られない場合は j++ する
+          (if (= k j)
+            (list i 0 (+ k 1))
+            ;j++ したいが、iがついて来られない場合は i++ する
+            (if (= j i)
+              (list 0 (+ j 1) k)
+              (list (+ i 1) j k)
             )
-
           )
+
         )
       )
       triple-index
@@ -23,4 +22,34 @@
   )
 )
 
-(stream-ref triple-index 1)
+(define (triples s t u)
+    (stream-map
+      (lambda (x)
+        (let ((i (car x)) (j (cadr x)) (k (caddr x)))
+          (list (stream-ref s i) (stream-ref t j) (stream-ref u k))
+        )
+      )
+      triple-index
+    )
+)
+
+(define (int n) (stream-cons n (int (+ n 1))))
+
+(define pythagorean-triples
+  (stream-filter
+    (lambda (x)
+      (let ((a (car x)) (b (cadr x)) (c (caddr x)))
+        (= (+ (* a a) (* b b)) (* c c))
+      )
+    )
+    (triples (int 1) (int 1) (int 1))
+  )
+)
+
+
+
+(stream-ref pythagorean-triples 0)
+(stream-ref pythagorean-triples 1)
+(stream-ref pythagorean-triples 2)
+(stream-ref pythagorean-triples 3)
+(stream-ref pythagorean-triples 4)
